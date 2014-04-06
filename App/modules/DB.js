cc42dbb4ae873926d80af1230f665aba6eaf4b66
David Wolf <d.wolf@live.at>
2014-04-06 16:29:49 +0200

    Register implementiert.

--- App/modules/DB.js
+++ App/modules/DB.js
@@ -5,13 +5,13 @@ var conString = 'postgres://admin:admin@localhost:5432/polaroid';
 
 exports.Login = function(username, password, callback)
 {
-	pg.connect(conString, function(err, client, done)  {
+	pg.connect(conString, function (err, client, done)  {
 		var query = client.query('select func_verify_user($1, $2) as retval', [username, password], function(err, result) {
 			if(err) {
 				callback(err);
 				return;
 			} else {
-				console.log('%s', result.rows[0].retval);
+				//console.log('%s', result.rows[0].retval);
 				callback(null, result);
 				done(); // Wichtig sonst werden Clients geleakt.
 			}
@@ -21,8 +21,18 @@ exports.Login = function(username, password, callback)
 
 exports.SignUp = function(first, last, user, mail, password, callback)
 {
-	// TODO: add database code
-	callback(null, null);
+	pg.connect(conString, function (error, client, done) {
+		// TODO: add XSS and SQL-Injection prevention
+		var query = client.query('select func_register_user($1, $2, $3, $4, $5) as retval', [mail, password, user, first, last], function (error, result) {
+			if (error) {
+				callback(error, null);
+				return;
+			} else {
+				callback(null, result);
+				done();
+			}
+		});
+	});
 }
 
 //Test
--- App/routing.js
+++ App/routing.js
@@ -17,13 +17,34 @@ function login (request, response)
 	});
 };
 
+function loginext (request, response, username, password)
+{
+	DB.Login(username, password, function(error, result) {
+		if(error) {
+			console.log(error);
+		} else {
+			if(result.rows[0].retval) {
+				request.session.user = result.rows[0].retval;
+				response.redirect(301, '/account');
+			} else {
+				response.set('error', 1);
+				response.redirect(301, '/');
+			}
+		}
+	});
+};
+
 function signup (request, response)
 {
 	var body = request.body;
 	DB.SignUp(body.first, body.last, body.user, body.mail, body.password, function(error, result) {
-		// TODO: add signup logic
-		response.set('error', 1);
-		response.redirect(301, '/');
+		if (error) {
+			response.set('error', 1);
+			response.redirect(301, '/');
+		} else {
+			// TODO: send confirmation mail
+			loginext(request, response, body.user, body.password);
+		}
 	});
 };
 
@@ -33,16 +54,16 @@ function create (request, response)
 };
 
 module.exports = function(app) {
-	app.post('/', function(request, response) {
+	app.post('/', function (request, response) {
 		login(request, response);
 		console.log(request.session);
 	});
-	app.post('/account/signup', function(request, response) {
+	app.post('/account/signup', function (request, response) {
 		signup(request, response);
 	})
 
 	// create button in login screen
-	app.get('/create', function(request, response) {
+	app.get('/create', function (request, response) {
 		create(request, response);
 	});
 };
