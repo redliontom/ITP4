8dc1032512639d0ee572bfc618e2da96480aa94a
David Wolf <d.wolf@live.at>
2014-04-08 21:39:06 +0200

    Cookies und Session wird korrekt gesetzt, jedoch noch nicht ausgelesen.

--- App/modules/DB.js
+++ App/modules/DB.js
@@ -4,16 +4,14 @@ var sanitizer = require('sanitizer');
 var conString = 'postgres://admin:admin@localhost:5432/polaroid';
 
 
-exports.Login = function(username, password, callback)
-{
+exports.login = function(username, password, callback) {
 	username = sanitizer.sanitize(username);
 	password = sanitizer.sanitize(password);
 
 	pg.connect(conString, function (err, client, done)  {
-		var query = client.query('select func_verify_user($1, $2) as retval', [username, password], function(err, result) {
+		var query = client.query('select func_verify_user($1, $2) as retval', [username, password], function (err, result) {
 			if(err) {
 				callback(err);
-				return;
 			} else {
 				//console.log('%s', result.rows[0].retval);
 				callback(null, result);
@@ -21,10 +19,9 @@ exports.Login = function(username, password, callback)
 			}
 		});
 	});
-}
+};
 
-exports.SignUp = function(first, last, user, mail, password, callback)
-{
+exports.signUp = function(first, last, user, mail, password, callback) {
 	first = sanitizer.sanitize(first);
 	last = sanitizer.sanitize(last);
 	user = sanitizer.sanitize(user);
@@ -35,16 +32,31 @@ exports.SignUp = function(first, last, user, mail, password, callback)
 		var query = client.query('select func_register_user($1, $2, $3, $4, $5) as retval', [mail, password, user, first, last], function (error, result) {
 			if (error) {
 				callback(error, null);
-				return;
 			} else {
 				callback(null, result);
 				done();
 			}
 		});
 	});
-}
+};
+
+exports.checkUser = function (username, password, callback) {
+	username = sanitizer.sanitize(username);
+	password = sanitizer.sanitize(password);
+
+	pg.connect(conString, function (error, client, done) {
+		var query = client.query('select func_verify_user_cookie($1, $2) as retval', [username, password], function (error, result) {
+			if (error) {
+				callback(error, null);
+			} else {
+				callback(null, result);
+				done();
+			}
+		});
+	});
+};
 
 //Test
-exports.Login('testuser', 'password', function(e) {
+exports.login('testuser', 'password', function(e) {
 	console.log(e);
 });
--- App/routing.js
+++ App/routing.js
@@ -1,13 +1,30 @@
 var DB = require('./modules/DB');
+var fs = require('fs');
 
-function login (request, response)
+function login (request, response, remember)
 {
-	DB.Login(request.body.username, request.body.password, function(error, result) {
+	loginext(request, response, request.body.username, request.body.password, request.body.remember);
+};
+
+function loginext (request, response, username, password, remember)
+{
+	if (!username || !password) {
+		response.redirect(301, '/');
+		return;
+	}
+	checkUser(request, response, username, password, request.cookies.polaroidRemember);
+	checkUser(request, response, username, password, request.session.polaroidHash);
+
+	DB.login(username, password, function (error, result) {
 		if(error) {
-			console.log(error);
+			logfile('error.log', error);
 		} else {
 			if(result.rows[0].retval != "null") {
-				request.session.user = result.rows[0].retval;
+				if (remember) {
+					response.cookie('polaroidRemember', result.rows[0].retval);
+				}
+				request.session.polaroidUser = username;
+				request.session.polaroidHash = result.rows[0].retval;
 				response.redirect(301, '/account');
 			} else {
 				response.set('error', 1);
@@ -17,19 +34,14 @@ function login (request, response)
 	});
 };
 
-function loginext (request, response, username, password)
+function checkUser (request, response, username, password, value)
 {
-	DB.Login(username, password, function(error, result) {
-		if(error) {
-			console.log(error);
-		} else {
-			if(result.rows[0].retval) {
-				request.session.user = result.rows[0].retval;
-				response.redirect(301, '/account');
-			} else {
-				response.set('error', 1);
-				response.redirect(301, '/');
-			}
+	DB.checkUser(username, password, function (error, result) {
+		if (error) {
+			logfile('error.log', error);
+			response.redirect(301, '/');
+		} else if (result == value) {
+			response.redirect(301, '/account');
 		}
 	});
 };
@@ -37,13 +49,14 @@ function loginext (request, response, username, password)
 function signup (request, response)
 {
 	var body = request.body;
-	DB.SignUp(body.first, body.last, body.user, body.mail, body.password, function(error, result) {
+
+	DB.signUp(body.first, body.last, body.user, body.mail, body.password, function (error, result) {
 		if (error) {
 			response.set('error', 1);
 			response.redirect(301, '/');
 		} else {
 			// TODO: send confirmation mail
-			loginext(request, response, body.user, body.password);
+			loginext(request, response, body.user, body.password, body.remember);
 		}
 	});
 };
@@ -75,6 +88,24 @@ function virtualLink (app)
 {
 	app.get('/create', function (request, response) {
 		create(request, response);
+	});
+};
+
+function logfile (path, message)
+{
+	console.log(error);
+	console.log(message);
+	return;
+	// TODO: vor auslieferung die ersten drei zeilen löschen
+
+	fs.open('error.log', 'a', 0666, function (error, fd) {
+		if (error) {
+			console.log(error);
+			console.log(message);
+		} else {
+			var buffer = new Buffer(message + '\n', 'utf8');
+			fs.writeSync(fd, buffer, 0, buffer.length, null);
+		}
 	});
 };
 
