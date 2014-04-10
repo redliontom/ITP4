1eb208375633f631c99cb0fb18b3d48f42722ab1
Leonhardt Schwarz <if12b076@technikum-wien.at>
2014-04-10 12:32:38 +0200

    bug fix?

--- App/routing.js
+++ App/routing.js
@@ -104,6 +104,7 @@ function linkLogout (request, response)
 	request.session = null;
 	response.cookie('polaroidRememberUser', null);
 	response.cookie('polaroidRememberHash', null);
+	response.header('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
 	response.redirect( '/');
 };
 
