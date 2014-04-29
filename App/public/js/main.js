f72a9bbc4d731df7c38103838784ba3f0da7dc32
redliontom <tomschalbar@hotmail.com>
2014-04-29 12:02:37 +0200

    Profile

--- App/public/js/main.js
+++ App/public/js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    
     $("#m-home").click(function() {
         $("#content_container").animate({opacity:"0", filter:"alpha(opacity=0)"}, 200, function() {
             $("#content_container").load("../account/feeds.html", function() {
@@ -14,4 +15,22 @@ $(document).ready(function() {
             });
         });
     });
+    
+    $("#m-profile").click(function() {
+        $("#content_container").animate({opacity:"0", filter:"alpha(opacity=0)"}, 200, function() {
+            $("#content_container").load("../account/profile.html", function() {
+               $("#content_container").animate({opacity:"1", filter:"alpha(opacity=100)"}, 200);
+            });
+        });
+    });
+    
+    $("#m-find-friends").click(function() {
+        $("#content_container").animate({opacity:"0", filter:"alpha(opacity=0)"}, 200, function() {
+            $("#content_container").load("../account/find.html", function() {
+               $("#content_container").animate({opacity:"1", filter:"alpha(opacity=100)"}, 200);
+            });
+        });
+    });
+    
 });
+
