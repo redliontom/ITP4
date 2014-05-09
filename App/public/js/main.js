d80d364a4e3b738de799bf4ad3f20c56a1d498e1
redliontom <tomschalbar@hotmail.com>
2014-05-09 11:29:33 +0200

    bug fix (header)

--- App/public/js/main.js
+++ App/public/js/main.js
@@ -33,5 +33,36 @@ $(document).ready(function() {
         });
     });
     
+    $("#m-mhome").click(function() {
+        $("#content_container").animate({opacity:"0", filter:"alpha(opacity=0)"}, 200, function() {
+            $("#content_container").load("../account/feeds.html", function() {
+               $("#content_container").animate({opacity:"1", filter:"alpha(opacity=100)"}, 200);
+            });
+        });
+    });
+    
+    $("#m-mgallery").click(function() {
+        $("#content_container").animate({opacity:"0", filter:"alpha(opacity=0)"}, 200, function() {
+            $("#content_container").load("../account/gallery.html", function() {
+               $("#content_container").animate({opacity:"1", filter:"alpha(opacity=100)"}, 200);
+            });
+        });
+    });
+    
+    $("#m-mprofile").click(function() {
+        $("#content_container").animate({opacity:"0", filter:"alpha(opacity=0)"}, 200, function() {
+            $("#content_container").load("../account/profile.html", function() {
+               $("#content_container").animate({opacity:"1", filter:"alpha(opacity=100)"}, 200);
+            });
+        });
+    });
+    
+    $("#m-mfind-friends").click(function() {
+        $("#content_container").animate({opacity:"0", filter:"alpha(opacity=0)"}, 200, function() {
+            $("#content_container").load("../account/find.html", function() {
+               $("#content_container").animate({opacity:"1", filter:"alpha(opacity=100)"}, 200);
+            });
+        });
+    });
 });
 
