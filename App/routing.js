f1c102b820bb05c78ba95988021a91fd3a4a8374
David Wolf <d.wolf@live.at>
2014-05-16 16:36:59 +0200

    Fehler ausgebessert.

--- App/routing.js
+++ App/routing.js
@@ -178,8 +178,7 @@ function linkLogin (request, response)
 
 function linkLogout (request, response)
 {
-	request.session.polaroidUser = null;
-	request.session.polaroidHash = null;
+	request.session = null;
 	response.cookie('polaroidRememberUser', null);
 	response.cookie('polaroidRememberHash', null);
 	//response.header('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
