9a156e61fdbac1e6d5170915de7af331b08b8579
redliontom <tomschalbar@hotmail.com>
2014-05-23 13:20:46 +0200

    Bild upload (provisorisch)

--- App/public/js/main.js
+++ App/public/js/main.js
@@ -101,6 +101,26 @@ $(document).ready(function() {
         profile();
     });
     
+    $("#m-upload").click(function() {
+        $("#content_container").animate({opacity:"0", filter:"alpha(opacity=0)"}, 200, function() {
+            $("#content_container").load("../account/upload.html", function() {
+               $("#content_container").animate({opacity:"1", filter:"alpha(opacity=100)"}, 200);
+            });
+        });
+        document.getElementById("subsection").innerHTML="Upload Image";
+        profile();
+    });
+    
+    $("#m-mupload").click(function() {
+        $("#content_container").animate({opacity:"0", filter:"alpha(opacity=0)"}, 200, function() {
+            $("#content_container").load("../account/upload.html", function() {
+               $("#content_container").animate({opacity:"1", filter:"alpha(opacity=100)"}, 200);
+            });
+        });
+        document.getElementById("subsection").innerHTML="Upload Image";
+        profile();
+    });
+    
 });
 
 
